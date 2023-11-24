29b272fb527e67cfc11c46f13b4e5c2ba7237db1
Fewakes <Slojkowski33@gmail.com>
2023-11-24 00:16:12 +0000

    Commit 2
    Designing About Me page.

---
+++ components/about.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section>
+      <h2>About me</h2>
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Management & Analytics</span>, I decided
+        to pursue my passion for programming. I enrolled for a variety of
+        courses and learned{" "}
+        <span className="font-medium">front-end web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect, as well as the never-ending 'chase' to learn new
+        things. I <span className="underline">love</span> the feeling of finally
+        figuring out a solution to a problem. My core stack is{" "}
+        <span className="font-medium">React.js</span>. I am also familiar with
+        TypeScript. I am always looking to learn new technologies. I am
+        currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        go-karting,watching f1, and playing with my cat. I also enjoy{" "}
+        <span className="font-medium">reading books</span>. My favourite author
+        is <span className="font-medium">Dan Brown</span>.
+      </p>
+    </section>
+  );
+}
