3a264d9224a658eda953defac72322b69a9e253a
Fewakes <Slojkowski33@gmail.com>
2023-11-27 14:57:51 +0000

    Commit 6
    Fixing some minor bugs and starting to implements contacts me section

--- components/experience.tsx
+++ components/experience.tsx
@@ -13,7 +13,7 @@ import { useSectionInView } from "@/lib/hooks";
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
   return (
-    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40" ref={ref}>
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,11 +1,11 @@
 import { useActiveSectionContext } from "@/context/active-section-context";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
-import { SectionName } from "./types";
+import type { SectionName } from "./types";
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const { ref, inView } = useInView({
-    threshold: threshold,
+    threshold,
   });
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
